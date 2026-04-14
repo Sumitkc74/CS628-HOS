d98fbf1358dce10c13b323b7a6aed1d412ce8df6
Sumitkc74 <120349298+Sumitkc74@users.noreply.github.com>
2026-04-14 19:30:54 +0000

    HOS03

---
+++ HOS03/hos03/src/App.js
@@ -0,0 +1,17 @@
+let guest = 0;
+
+function App() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
+
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default App;
